517ce9f55eeecc23e05660b23bc54b6d9378e37a
wittend <wittend@wwrinc.com>
2020-12-09 14:23:48 -0600

    Make certain (I hope) that -U delay occurs BEFORE DRDY is checked.

--- cmdmgr.c
+++ cmdmgr.c
@@ -720,7 +720,7 @@ int getCommandLine(int argc, char** argv, pList *p)
                 fprintf(stdout, "   -S                     :  Site prefix string for log files.     [ 32 char max. Do not use /\'\"* etc. Try callsign! ]\n");
                 //fprintf(stdout, "   -t                     :  Set CMM Data Rate.                    [ 96 hex default ]\n");
                 fprintf(stdout, "   -T                     :  Raw timestamp in milliseconds.        [ default: UTC string ]\n");
-                fprintf(stdout, "   -U <delay as ms>       :  Delay in mSec after DRDY.             [ default: 0 ]\n");
+                fprintf(stdout, "   -U <delay as ms>       :  Delay in mSec before DRDY.            [ default: 0 ]\n");
                 fprintf(stdout, "   -V                     :  Display software version and exit.\n");
                 fprintf(stdout, "   -X                     :  Read Simple Magnetometer Board (SMSB).\n");
                 fprintf(stdout, "   -x                     :  Read board with extender (MSBx).\n");
